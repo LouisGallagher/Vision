24d64bbcbcddb0e38a50e1dbdf616a25f79aa9cd
Louis Gallagher <louis.gallagher@hotmail.co.uk>
2015-11-16 17:44:45 +0000

    camera sim working with matplotlib

--- AugReal/CalibCamera/camera.py
+++ AugReal/CalibCamera/camera.py
@@ -1,5 +1,6 @@
-import cv2, numpy as np, math as m
-
+import cv2, numpy as np 
+import math as m
+import matplotlib.pyplot as plt
 
 #-----------Section: Rigid body transformations-----------#
 
@@ -12,15 +13,15 @@ import cv2, numpy as np, math as m
 # gamma: angle to rotate about the z axis
 def eulerToR(alpha, beta, gamma):
 		
-	alphaRad, betaRad, gammaRad = np.radians(([alpha, beta, gamma])) # convert degrees to radians
+	#alphaRad, betaRad, gammaRad = np.radians(([alpha, beta, gamma])) # convert degrees to radians
 	
 	## perform trig functions
-	cosAlpha = int(m.degrees(m.cos(alphaRad)))
-	sinAlpha = int(m.degrees(m.sin(alphaRad)))
-	cosBeta  = int(m.degrees(m.cos(betaRad)))
-	sinBeta  = int(m.degrees(m.sin(betaRad)))
-	cosGamma = int(m.degrees(m.cos(gammaRad)))
-	sinGamma = int(m.degrees(m.sin(gammaRad)))
+	cosAlpha = m.cos(alpha)
+	sinAlpha = m.sin(alpha)
+	cosBeta  = m.cos(beta)
+	sinBeta  = m.sin(beta)
+	cosGamma = m.cos(gamma)
+	sinGamma = m.sin(gamma)
 	
 	# compute rotation matrices for rotating about x, y and z axes 
 	rx = np.array([[1, 0, 0], [0, cosAlpha, -1 * sinAlpha], [0, sinAlpha, cosAlpha]])  
@@ -28,9 +29,6 @@ def eulerToR(alpha, beta, gamma):
 	rz = np.array([[cosGamma, -1 * sinGamma, 0], [sinGamma, cosGamma, 0], [0, 0, 1]])  
 	
 	return np.dot(rz, np.dot(ry, rx))  # multiply together for roll, pitch and yaw
-
-
-
 
 
 # function: computes a rotation matrix based on Rodriguez formula 
@@ -96,40 +94,68 @@ def intrinsicsToK(f, k, l, u0, v0):
 		return np.array([[f * k, 0, u0 , 0], [0, f * l, v0 , 0], [0, 0, 1, 0]])
 
 
-def simulateCamera(f, h, d, ch):
-	imagePlane = np.array([[[255 for i in range(3)] for x in range(h)] for y in range(d)], dtype = np.uint8)
-	K  = intrinsicsToK(f, 1, 1,  int(h/2), int(d/2) ) ##something not right with h and d  
-	#K = np.delete(K, (-1), axis = 1)
-	T = eulerToT(0, 0 , ch, 0, 90, -90)
-	#T = np.delete(T, (-1), axis = 0)
-	M = np.dot(K, T)
-	#M = np.delete(M, (2), axis = 1)
+# def simulateCamera(f, h, d, ch):
+# 	imagePlane = np.array([[[255 for i in range(3)] for x in range(h)] for y in range(d)], dtype = np.uint8)
+# 	K  = intrinsicsToK(f, 1, 1,  int(h/2), int(d/2) ) ##something not right with h and d  
+# 	#K = np.delete(K, (-1), axis = 1)
+# 	T = eulerToT(0, 0 , ch, 0, 90, -90)
+# 	#T = np.delete(T, (-1), axis = 0)
+# 	M = np.dot(K, T)
+# 	#M = np.delete(M, (2), axis = 1)
 	
 	
-	for i in range(1000):
-		pl1Image = np.dot(M, np.array([[i,1,0,1]]).T)
-		pl2Image = np.dot(M, np.array([[i,-1,0,1]]).T)
-		pl3Image = np.dot(M, np.array([[i,0,0,1]]).T)
-		x1im, y1im = [int(x / pl1Image[2]) for x in pl1Image[:2]]
-		x2im, y2im = [int(x / pl2Image[2]) for x in pl2Image[:2]]
-		x3im, y3im = [int(x / pl3Image[2]) for x in pl3Image[:2]]
+# 	for i in range(1000):
+# 		pl1Image = np.dot(M, np.array([[i,1,0,1]]).T)
+# 		pl2Image = np.dot(M, np.array([[i,-1,0,1]]).T)
+# 		pl3Image = np.dot(M, np.array([[i,0,0,1]]).T)
+# 		x1im, y1im = [int(x / pl1Image[2]) for x in pl1Image[:2]]
+# 		x2im, y2im = [int(x / pl2Image[2]) for x in pl2Image[:2]]
+# 		x3im, y3im = [int(x / pl3Image[2]) for x in pl3Image[:2]]
 
-		if 0 <= y1im < h and 0<= x1im < d:	 		
-			imagePlane[y1im , x1im] = [255,0,0]
+# 		if 0 <= y1im < h and 0<= x1im < d:	 		
+# 			imagePlane[y1im , x1im] = [255,0,0]
 
-		if 0 <= y2im < h and 0 <= x2im< d:	 		
-			imagePlane[y2im , x2im] = [0,0,255]
+# 		if 0 <= y2im < h and 0 <= x2im< d:	 		
+# 			imagePlane[y2im , x2im] = [0,0,255]
 
-		# if 0 <= y3im < h and 0 <= x3im< d:	 		
-		# 	imagePlane[y3im , x3im] = [0,0,255]	
+# 		if 0 <= y3im < h and 0 <= x3im< d:	 		
+# 			imagePlane[y3im , x3im] = [0,0,255]	
 	
-	return imagePlane
+# 	return imagePlane
 
-def runSim():
-	f = 1
-	while cv2.waitKey(2000) != ord('q'):
-		im = simulateCamera(f, 480, 360, -2000)
-		cv2.imshow('img' , im)
-		f += 10
 
-	cv2.destroyAllWindows()
+
+def simulateCamera():
+	T = eulerToT(0, 0, -20, 0, np.pi/2, -1 * (np.pi/2))	
+	K = intrinsicsToK(500, 1, 1, 0, 0)
+	M = K.dot(T)
+	M = np.delete(M, (2), axis = 1)
+	
+	fig = plt.figure()
+	ax = fig.gca() 
+
+	for i in range(100):
+		p1 = M.dot(np.array([[i, 1, 1]]).T)
+		p2 = M.dot(np.array([[i, 0, 1]]).T)
+		p3 = M.dot(np.array([[i, -1, 1]]).T)
+		ax.plot(p1[0]/p1[2],p1[1]/p1[2],'r.')
+		ax.plot(p2[0]/p2[2],p2[1]/p2[2],'k.')
+		ax.plot(p3[0]/p3[2],p3[1]/p3[2],'g.')
+
+	
+	
+
+	# # e1 = np1.hstack(np.array([[i, 1, 1]]).T for i in range(10))
+	# # e2 = np.hstack(np.array([[i, 0, 1]]).T for i in range(10))
+	# # e3 = np.hstack(np.array([[i, -1, 1]]).T for i in range(10))
+
+	
+	# i1 = M.dot(e1)
+	# i2 = M.dot(e2)
+	# i3 = M.dot(e3)
+
+
+	
+	# ax.plot(p1[0]/p1[2],p1[1]/p1[2],'r.')
+	plt.show()
+
